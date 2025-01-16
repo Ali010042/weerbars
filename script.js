cede540c7963ece567ac24b4efdb9086219ae71b
Rubens120 <9014886@student.zadkine.nl>
2025-01-16 10:29:14 +0100

    apis
    api

--- script.js
+++ script.js
@@ -2,14 +2,71 @@ let cityInput = document.getElementById('city_input'),
     searchBtn = document.getElementById('searchBtn'),
     api_key = '681b45cb415365cab9ca972bd397c291',
     currentWeatherCard = document.querySelectorAll('.weer-linker .kaart')[0],
-    fiveDaysForecastContainer = document.querySelector('.dag-voorspelling');
+    fiveDaysForecastContainer = document.querySelector('.dag-voorspelling'),
+    aqiCard = document.querySelectorAll('.highlights .kaart')[0],
+    sunriseCard = document.querySelectorAll('.highlights .kaart')[1],
+    neerslagkans = document.getElementById('neerslagkans'),
+    luchtdruk = document.getElementById('luchtdruk'),
+    zicht = document.getElementById('zicht'),
+    snelheid = document.getElementById('snelheid'),
+    windsnelheid = document.getElementById('windsnelheid'),
+
+    aqiList = ['Goed', 'Prima', 'Oke', 'Slecht', 'Heel slecht'];
 
 function getWeatherDetails(name, lat, lon, country, state) {
     let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
         WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+        AIR_POLLUTION_API_URL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}`,
+
         days = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag'],
         months = ['Jan', 'Feb', 'Mar', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'];
 
+        fetch(AIR_POLLUTION_API_URL).then(res => res.json()).then(data => {
+            let {co, no, no2, o3, so2, pm2_5, pm10, nh3} = data.list[0].components;
+            aqiCard.innerHTML = 
+            `  <div class="kaart-hoofd">
+                <p>Luchtkwaliteit</p>
+                <p class="air-index aqi-${data.list[0].main.aqi}">${aqiList[data.list[0].main.aqi - 1]}</p>
+            </div>
+            <div class="air-indices">
+                <i class="fa-regular fa-wind fa-3x"></i>
+                <div class="item">
+                    <p>PM2.5</p>
+                    <h2>${pm2_5}</h2>
+                </div>
+                <div class="item">
+                    <p>PM10</p>
+                    <h2>${pm10}</h2>
+                </div>
+                <div class="item">
+                    <p>SO2</p>
+                    <h2>${so2}</h2>
+                </div>
+                <div class="item">
+                    <p>CO</p>
+                    <h2>${co}</h2>
+                </div>
+                <div class="item">
+                    <p>NO</p>
+                    <h2>${no}</h2>
+                </div>
+                <div class="item">
+                    <p>NO2</p>
+                    <h2>${no2}</h2>
+                </div>
+                <div class="item">
+                    <p>NH3</p>
+                    <h2>${nh3}</h2>
+                </div>
+                <div class="item">
+                    <p>O3</p>
+                    <h2>${o3}</h2>
+            </div>
+        </div>
+    </div>`
+        }).catch(() => {
+            alert('kan weer niet laten zien')
+        })
 
     fetch(WEATHER_API_URL)
         .then(res => res.json())
@@ -31,6 +88,42 @@ function getWeatherDetails(name, lat, lon, country, state) {
                     <p><i class="fa-light fa-calendar"></i> ${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}</p>
                     <p><i class="fa-light fa-location-dot"></i> ${name}, ${country}</p>
                 </div>`;
+                let {sunrise, sunset} = data.sys,
+                 {timezone , visibility} = data,
+                 {humidity, pressure, feels_like} = data.main,
+                 {speed} = data.wind,
+                sRiseTime = moment.utc(sunrise, 'X').add(timezone, 'seconds').format('hh:mm A'),
+                sSetTime = moment.utc(sunset, 'X').add(timezone, 'seconds').format('hh:mm A')
+                sunriseCard.innerHTML = `
+                  <div class="kaart-hoofd">
+                           <p>Zondsopgang en Zondsondergang</p>
+                    </div>
+                    <div class="zondsopgang-ondergang">
+                        <div class="item">
+                            <div class="icon">
+                                <i class="fa-light fa-sunrise fa-4x"></i>
+                            </div>
+                            <div>
+                                <p>Zondsopgang</p>
+                                <h2>${sRiseTime}</h2>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="icon">
+                                <i class="fa-light fa-sunset fa-4x"></i>
+                            </div>
+                            <div>
+                                <p>Zondsondergang</p>
+                                <h2>${sSetTime}</h2>
+                            </div>
+                        </div>
+                    </div>
+                        `;
+                    neerslagkans.innerHTML = `${neerslagkans}%`;
+                    luchtdruk.innerHTML = `${luchtdruk}hPa`;
+                    zicht.innerHTML = `${zicht / 1000}km`;
+                    snelheid.innerHTML = `${snelheid}m/s`;
+                    windsnelheid.innerHTML = `${(feels_like - 273.15).toFixed(2)}&deg;C`;
         })
         .catch(() => {
            //alert('Gefaald om het huidige weer weer te geven');
